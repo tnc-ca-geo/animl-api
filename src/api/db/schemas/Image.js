a1a55223390261d1094a479bb4bd5e7042a7cecd
Nathaniel Rindlaub <nathanielrindlaub@gmail.com>
2022-07-14 21:22:09 -0700

    Fix image dupe checking bug, #60

--- src/api/db/schemas/Image.js
+++ src/api/db/schemas/Image.js
@@ -9,42 +9,42 @@ const Schema = mongoose.Schema;
  *    userSetData  - user configured EXIF data
  */
 
-let ImageSchema = new Schema({
-  _id: { type: String, required: true },
-  bucket: { type: String, required: true },
-  fileTypeExtension: { type: String, required: true },
-  dateAdded: { type: Date, default: Date.now, required: true },
-  dateTimeOriginal: { type: Date, required: true },
-  // TODO: add dateTimeUTC field
-  make: { type: String, default: 'unknown', required: true },
-  cameraId: { type: String, required: true, ref: 'Camera' },
-  deploymentId: { type: Schema.Types.ObjectId, ref: 'Deployment', required: true },
-  projectId: { type: String, required: true, ref: 'Project' },
-  originalFileName: { type: String },
-  imageWidth: { type: Number },
-  imageHeight: { type: Number },
-  mimeType: { type: String },
-  userSetData: { type: Map, of: String },
-  model: { type: String },
-  location: { type: shared.LocationSchema },
-  objects: { type: [shared.ObjectSchema] },
+const ImageSchema = new Schema({
+    _id: { type: String, required: true },
+    bucket: { type: String, required: true },
+    fileTypeExtension: { type: String, required: true },
+    dateAdded: { type: Date, default: Date.now, required: true },
+    dateTimeOriginal: { type: Date, required: true },
+    // TODO: add dateTimeUTC field
+    make: { type: String, default: 'unknown', required: true },
+    cameraId: { type: String, required: true, ref: 'Camera' },
+    deploymentId: { type: Schema.Types.ObjectId, ref: 'Deployment', required: true },
+    projectId: { type: String, required: true, ref: 'Project' },
+    originalFileName: { type: String },
+    imageWidth: { type: Number },
+    imageHeight: { type: Number },
+    mimeType: { type: String },
+    userSetData: { type: Map, of: String },
+    model: { type: String },
+    location: { type: shared.LocationSchema },
+    objects: { type: [shared.ObjectSchema] }
 });
 
 ImageSchema.index(
-  // TODO: not sure we really need to index by cameraId if we're indexing by 
-  // deployment.
-  // Also, currently we're sorting date in decending order (-1), 
-  // (newest images first) but the front end is requesting oldest first by 
-  // default. 
-  // Also we should update this when we add dateTimeUTC field. 
-  { cameraId: 1, dateTimeOriginal: -1 },
-  { unique: true, sparse: true }
+    // TODO: revisit indexing. I'm not sure we really need to index by 
+    // cameraId if we're indexing by deployment.
+    // Also, currently we're sorting date in decending order (-1),
+    // (newest images first) but the front end is requesting oldest first by
+    // default.
+    // Also we should update this when we add dateTimeUTC field.
+    { cameraId: 1, dateTimeOriginal: -1 },
+    { sparse: true }
 );
 ImageSchema.index({ deploymentId: 1 });
 ImageSchema.index({ projectId: 1 });
 
 ImageSchema.on('index', (e) => {
-  console.log('Indexing error', e);
+    console.log('Indexing error', e);
 });
 
 ImageSchema.plugin(MongoPaging.mongoosePlugin);
