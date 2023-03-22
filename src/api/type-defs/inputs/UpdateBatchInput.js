f76255383053ec7fa59e5937dcf8c9b4f8e03a84
ingalls <nick@ingalls.ca>
2023-03-22 11:07:22 -0600

    Update required Batch props on update

--- src/api/type-defs/inputs/UpdateBatchInput.js
+++ src/api/type-defs/inputs/UpdateBatchInput.js
@@ -1,10 +1,10 @@
 module.exports = `
   input UpdateBatchInput {
     _id: String!
-    eTag: String!
+    eTag: String
     total: Int
     processingEnd: String
-    processingStart: String!
+    processingStart: String
     originalFile: String
     uploadedFile: String
 }`;
