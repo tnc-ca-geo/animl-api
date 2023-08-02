9d0c15bee6d98ba209b0c4cb672119edee1f4a79
ingalls <nick@ingalls.ca>
2023-08-02 08:06:52 -0600

    Use Model from ImageError Export

--- src/api/db/models/BatchError.js
+++ src/api/db/models/BatchError.js
@@ -5,64 +5,64 @@ import retry from 'async-retry';
 import { hasRole } from './utils.js';
 
 export class BatchErrorModel {
-    static async createError(input) {
-      const operation = async (input) => {
-        return await retry(async () => {
-          const newBatchError = new BatchError(input);
-          await newBatchError.save();
-          return newBatchError;
-        }, { retries: 2 });
+  static async createError(input) {
+    const operation = async (input) => {
+      return await retry(async () => {
+        const newBatchError = new BatchError(input);
+        await newBatchError.save();
+        return newBatchError;
+      }, { retries: 2 });
+    };
+
+    try {
+      const batcherr = await operation({
+        batch: input.batch,
+        error: input.error
+      });
+
+      return {
+        _id: batcherr._id,
+        batch: batcherr.batch,
+        error: batcherr.error,
+        created: batcherr.created
       };
-
-      try {
-        const batcherr = await operation({
-          batch: input.batch,
-          error: input.error
-        });
-
-        return {
-          _id: batcherr._id,
-          batch: batcherr.batch,
-          error: batcherr.error,
-          created: batcherr.created
-        };
-      } catch (err) {
-        // if error is uncontrolled, throw new ApolloError
-        if (err instanceof ApolloError) throw err;
-        throw new ApolloError(err);
-      }
+    } catch (err) {
+      // if error is uncontrolled, throw new ApolloError
+      if (err instanceof ApolloError) throw err;
+      throw new ApolloError(err);
     }
+  }
 
-    static async clearErrors(input) {
-      const operation = async (input) => {
-        return await retry(async () => {
-          return await BatchError.deleteMany(input);
-        }, { retries: 2 });
-      };
+  static async clearErrors(input) {
+    const operation = async (input) => {
+      return await retry(async () => {
+        return await BatchError.deleteMany(input);
+      }, { retries: 2 });
+    };
 
-      try {
-        await operation({
-          batch: input.batch
-        });
+    try {
+      await operation({
+        batch: input.batch
+      });
 
-        return { message: 'Cleared' };
-      } catch (err) {
-        // if error is uncontrolled, throw new ApolloError
-        if (err instanceof ApolloError) throw err;
-        throw new ApolloError(err);
-      }
+      return { message: 'Cleared' };
+    } catch (err) {
+      // if error is uncontrolled, throw new ApolloError
+      if (err instanceof ApolloError) throw err;
+      throw new ApolloError(err);
     }
+  }
 }
 
 const generateBatchErrorModel = ({ user } = {}) => ({
   get createError() {
     if (!hasRole(user, WRITE_IMAGES_ROLES)) throw new ForbiddenError;
-    return BatchErrorModel.createError
+    return BatchErrorModel.createError;
   },
 
   get clearErrors() {
     if (!hasRole(user, WRITE_IMAGES_ROLES)) throw new ForbiddenError;
-    return BatchErrorModel.clearErrors(input);
+    return BatchErrorModel.clearErrors;
   }
 });
 
