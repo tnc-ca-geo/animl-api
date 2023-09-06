f60e39f03c2c2778c0f792c38feddc7a9519ea19
ingalls <nick@ingalls.ca>
2023-09-06 14:48:03 +0100

    Make lints cover sub dirs of test

--- test/lib/config.js
+++ test/lib/config.js
@@ -5,44 +5,44 @@ import SSM from '@aws-sdk/client-ssm';
 import Mongoose from 'mongoose';
 
 export class MockMongoose {
-    constructor() {
-        this.responses = [];
-    }
+  constructor() {
+    this.responses = [];
+  }
 }
 
-export default function() {
-    Sinon.stub(SSM.SSMClient.prototype, 'send').callsFake((command) => {
-        if (command instanceof SSM.GetParametersCommand) {
-            const Parameters = command.input.Names.map((name) => {
-                return {
-                    Name: name,
-                    Value: '123'
-                }
-            });
+export default function(t) {
+  Sinon.stub(SSM.SSMClient.prototype, 'send').callsFake((command) => {
+    if (command instanceof SSM.GetParametersCommand) {
+      const Parameters = command.input.Names.map((name) => {
+        return {
+          Name: name,
+          Value: '123'
+        };
+      });
 
-            Parameters.push({
-                Name: '/EXPORTS/EXPORTED_DATA_BUCKET',
-                Value: 'example-bucket'
-            });
+      Parameters.push({
+        Name: '/EXPORTS/EXPORTED_DATA_BUCKET',
+        Value: 'example-bucket'
+      });
 
-            return Promise.resolve({ Parameters, InvalidParameters: [] });
-        } else {
-            t.fail();
-        }
-    });
+      return Promise.resolve({ Parameters, InvalidParameters: [] });
+    } else {
+      t.fail();
+    }
+  });
 
-    Sinon.stub(SM.SecretsManagerClient.prototype, 'send').callsFake((command) => {
-        if (command instanceof SM.GetSecretValueCommand) {
-            return Promise.resolve({
-                SecretString: '{"apikey": "api-key"}'
-            })
-        } else {
-            t.fail();
-        }
-    });
+  Sinon.stub(SM.SecretsManagerClient.prototype, 'send').callsFake((command) => {
+    if (command instanceof SM.GetSecretValueCommand) {
+      return Promise.resolve({
+        SecretString: '{"apikey": "api-key"}'
+      });
+    } else {
+      t.fail();
+    }
+  });
 
-    const m = new MockMongoose();
-    Sinon.stub(Mongoose, 'connect').callsFake((command) => {
-        return Promise.resolve(m);
-    });
+  const m = new MockMongoose();
+  Sinon.stub(Mongoose, 'connect').callsFake(() => {
+    return Promise.resolve(m);
+  });
 }
