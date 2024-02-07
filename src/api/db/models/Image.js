74eb9074bfa3d4b0a5d48aa50c2b37c769285fa2
ingalls <nick@ingalls.ca>
2024-02-06 19:37:18 -0700

    A couple count fixes

--- src/api/db/models/Image.js
+++ src/api/db/models/Image.js
@@ -51,11 +51,11 @@ export class ImageModel {
     return res[0] ? res[0].count : 0;
   }
 
-  static countImagesByLabel(labels, context) {
+  static async countImagesByLabel(labels, context) {
       const pipeline = [
         { '$match': { 'projectId': context.user['curr_project'] } },
-        buildLabelPipeline(labels),
-        { $count: 'count' });
+        ...buildLabelPipeline(labels),
+        { $count: 'count' }
       ]
 
     const res = await Image.aggregate(pipeline);
