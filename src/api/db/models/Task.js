bf835b029946757cf2985ab4e4e158ead7437b05
ingalls <nick@ingalls.ca>
2024-03-12 13:42:31 -0600

    Add Create & Query By Id

--- src/api/db/models/Task.js
+++ src/api/db/models/Task.js
@@ -1,4 +1,4 @@
-import GraphQLError, { InternalServerError, ForbiddenError, AuthenticationError } from '../../errors.js';
+import GraphQLError, { NotFoundError, InternalServerError, ForbiddenError, AuthenticationError } from '../../errors.js';
 import MongoPaging from 'mongo-cursor-pagination';
 import Task from '../schemas/Task.js';
 import { hasRole } from './utils.js';
@@ -23,22 +23,35 @@ export class TaskModel {
    * @param {Object} context
    */
   static async queryByFilter(input, context) {
-    try {
-      return await MongoPaging.aggregate(Task.collection, {
+    return await MongoPaging.aggregate(Task.collection, {
         aggregation: [
-          { '$match': { 'projectId': context.user['curr_project'] } },
-          { '$match': { 'user': context.user.sub } }
+            { '$match': { 'projectId': context.user['curr_project'] } },
+            { '$match': { 'user': context.user.sub } }
         ],
         limit: input.limit,
         paginatedField: input.paginatedField,
         sortAscending: input.sortAscending,
         next: input.next,
         previous: input.previous
-      });
-    } catch (err) {
-      if (err instanceof GraphQLError) throw err;
-      throw new InternalServerError(err);
+    });
+  }
+
+  static async queryById(_id, context) {
+    const query = { _id };
+    const task = await Task.findOne(query);
+    if (!task) throw new NotFoundError('Task not found');
+
+    if (task.projectId !== context.user['curr_project']) {
+        throw new NotFoundError('Task does not belong to current project');
     }
+
+    return task;
+  }
+
+  static async create(input) {
+    const task = new Task(input);
+    await task.save();
+    return task;
   }
 }
 
@@ -46,6 +59,12 @@ export default class AuthedTaskModel {
   constructor(user) {
     if (!user) throw new AuthenticationError('Authentication failed');
     this.user = user;
+  }
+
+  async queryById(input, context) {
+    if (!hasRole(this.user, READ_TASKS_ROLES)) throw new ForbiddenError();
+
+    return await TaskModel.queryById(input, context);
   }
 
   async queryByFilter(input, context) {
