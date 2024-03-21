e7968f16556a535536b0f82b51fdef88abea4e4f
ingalls <nick@ingalls.ca>
2024-03-21 10:13:52 -0600

    Use signer directly

--- src/export/image-errors.js
+++ src/export/image-errors.js
@@ -3,7 +3,7 @@ import stream from 'node:stream/promises';
 import { PassThrough } from 'node:stream';
 import { Upload } from '@aws-sdk/lib-storage';
 import S3 from '@aws-sdk/client-s3';
-import Signer from './signer.js';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { InternalServerError } from '../api/errors.js';
 import { stringify } from 'csv-stringify';
 import ImageError from '../api/db/schemas/ImageError.js';
