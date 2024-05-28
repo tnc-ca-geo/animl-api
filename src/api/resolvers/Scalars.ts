310b26b57dd05e637a9cadfe4868f97db9ffa537
Anthony Lukach <anthonylukach@gmail.com>
2024-05-28 13:41:20 -0700

    Convert scalars.js

--- src/api/resolvers/Scalars.ts
+++ src/api/resolvers/Scalars.ts
@@ -1,5 +1,5 @@
 import { DateTime } from 'luxon';
-import { GraphQLScalarType } from 'graphql';
+import { GraphQLScalarType, type StringValueNode } from 'graphql';
 import GraphQLJSON from 'graphql-type-json';
 
 const JSONObject = GraphQLJSON.GraphQLJSONObject;
@@ -11,23 +11,22 @@ const JSONObject = GraphQLJSON.GraphQLJSONObject;
 const Date = new GraphQLScalarType({
   name: 'Date',
   description: 'Date scalar type',
-  parseValue(value) {
+  parseValue(value: unknown) {
     // Parse input when the type of input is JSON
     // e.g. input is passed into query as a JSON variable
-    return DateTime.fromISO(value);
+    return DateTime.fromISO(value as string);
   },
   serialize(value) {
     // Prep return value to be sent to client
-    return DateTime.fromJSDate(value).toISO();
+    return DateTime.fromJSDate(value as Date).toISO();
   },
   parseLiteral(ast) {
     // Parse input when the type of input is "inline" (AST)
-    return DateTime.fromISO(ast.value);
-  }
+    return DateTime.fromISO((ast as StringValueNode).value);
+  },
 });
 
 export default {
   JSONObject,
-  Date
+  Date,
 };
-
