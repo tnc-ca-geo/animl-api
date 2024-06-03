ed4b163f08a3a1ceb340229bda0460757680efd5
Anthony Lukach <anthonylukach@gmail.com>
2024-06-03 16:27:02 -0700

    Type scalars

--- src/api/resolvers/Scalars.ts
+++ src/api/resolvers/Scalars.ts
@@ -1,6 +1,6 @@
 import { DateTime } from 'luxon';
 import { GraphQLScalarType } from 'graphql';
-import GraphQLJSON from 'graphql-type-json';
+import * as GraphQLJSON from 'graphql-type-json';
 
 const JSONObject = GraphQLJSON.GraphQLJSONObject;
 
@@ -12,22 +12,28 @@ const Date = new GraphQLScalarType({
   name: 'Date',
   description: 'Date scalar type',
   parseValue(value) {
+    if (typeof value !== 'string') throw new Error(`Expected string, got ${typeof value}`);
     // Parse input when the type of input is JSON
     // e.g. input is passed into query as a JSON variable
     return DateTime.fromISO(value);
   },
   serialize(value) {
+    if (!isDate(value)) throw new Error(`Expected string, got ${typeof value}`);
     // Prep return value to be sent to client
     return DateTime.fromJSDate(value).toISO();
   },
   parseLiteral(ast) {
+    if (ast.kind !== 'StringValue') throw new Error(`Expected string, got ${ast.kind}`);
     // Parse input when the type of input is "inline" (AST)
     return DateTime.fromISO(ast.value);
-  }
+  },
 });
 
 export default {
   JSONObject,
-  Date
+  Date,
 };
 
+function isDate(val: unknown): val is Date {
+  return val instanceof global.Date;
+}
