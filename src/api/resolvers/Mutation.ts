286f7e693bd4078c709754ade5c667adbc923643
Anthony Lukach <anthonylukach@gmail.com>
2024-06-04 11:05:47 -0700

    Add return types

--- src/api/resolvers/Mutation.ts
+++ src/api/resolvers/Mutation.ts
@@ -6,7 +6,7 @@ export default {
     _: unknown,
     { input }: gql.MutationCreateBatchErrorArgs,
     context: Context,
-  ) => {
+  ): Promise<gql.BatchError> => {
     const error = await context.models.BatchError.createError(input);
     return { ...error };
   },
@@ -15,7 +15,7 @@ export default {
     _: unknown,
     { input }: gql.MutationCreateImageErrorArgs,
     context: Context,
-  ) => {
+  ): Promise<gql.ImageError> => {
     const error = await context.models.ImageError.createError(input);
     return { ...error };
   },
@@ -24,7 +24,7 @@ export default {
     _: unknown,
     { input }: gql.MutationClearImageErrorsArgs,
     context: Context,
-  ) => {
+  ): Promise<gql.StandardPayload> => {
     const res = await context.models.ImageError.clearErrors(input);
     return { ...res };
   },
@@ -33,42 +33,70 @@ export default {
     _: unknown,
     { input }: gql.MutationClearBatchErrorsArgs,
     context: Context,
-  ) => {
+  ): Promise<gql.StandardPayload> => {
     const res = await context.models.BatchError.clearErrors(input);
     return { ...res };
   },
 
-  createUpload: async (_: unknown, { input }: gql.MutationCreateUploadArgs, context: Context) => {
+  createUpload: async (
+    _: unknown,
+    { input }: gql.MutationCreateUploadArgs,
+    context: Context,
+  ): Promise<gql.CreateUploadPayload> => {
     const res = await context.models.Batch.createUpload(input, context);
     return { ...res };
   },
 
-  closeUpload: async (_: unknown, { input }: gql.MutationCloseUploadArgs, context: Context) => {
+  closeUpload: async (
+    _: unknown,
+    { input }: gql.MutationCloseUploadArgs,
+    context: Context,
+  ): Promise<gql.StandardPayload> => {
     const res = await context.models.Batch.closeUpload(input);
     return { ...res };
   },
 
-  createUser: async (_: unknown, { input }: gql.MutationCreateUserArgs, context: Context) => {
+  createUser: async (
+    _: unknown,
+    { input }: gql.MutationCreateUserArgs,
+    context: Context,
+  ): Promise<gql.StandardPayload> => {
     const res = await context.models.User.createUser(input, context);
     return { ...res };
   },
 
-  updateUser: async (_: unknown, { input }: gql.MutationUpdateUserArgs, context: Context) => {
+  updateUser: async (
+    _: unknown,
+    { input }: gql.MutationUpdateUserArgs,
+    context: Context,
+  ): Promise<gql.StandardPayload> => {
     const res = await context.models.User.updateUser(input, context);
     return { ...res };
   },
 
-  updateBatch: async (_: unknown, { input }: gql.MutationUpdateBatchArgs, context: Context) => {
+  updateBatch: async (
+    _: unknown,
+    { input }: gql.MutationUpdateBatchArgs,
+    context: Context,
+  ): Promise<gql.BatchPayload> => {
     const batch = await context.models.Batch.updateBatch(input);
     return { batch };
   },
 
-  redriveBatch: async (_: unknown, { input }: gql.MutationRedriveBatchArgs, context: Context) => {
+  redriveBatch: async (
+    _: unknown,
+    { input }: gql.MutationRedriveBatchArgs,
+    context: Context,
+  ): Promise<gql.StandardPayload> => {
     const res = await context.models.Batch.redriveBatch(input);
     return { ...res };
   },
 
-  stopBatch: async (_: unknown, { input }: gql.MutationStopBatchArgs, context: Context) => {
+  stopBatch: async (
+    _: unknown,
+    { input }: gql.MutationStopBatchArgs,
+    context: Context,
+  ): Promise<gql.StandardPayload> => {
     const res = await context.models.Batch.stopBatch(input);
     return { ...res };
   },
@@ -77,7 +105,7 @@ export default {
     _: unknown,
     { input }: gql.MutationDeleteImageCommentArgs,
     context: Context,
-  ) => {
+  ): Promise<gql.ImageCommentsPayload> => {
     const res = await context.models.Image.deleteComment(input, context);
     return { ...res };
   },
@@ -86,7 +114,7 @@ export default {
     _: unknown,
     { input }: gql.MutationUpdateImageCommentArgs,
     context: Context,
-  ) => {
+  ): Promise<gql.ImageCommentsPayload> => {
     const res = await context.models.Image.updateComment(input, context);
     return { ...res };
   },
@@ -95,17 +123,25 @@ export default {
     _: unknown,
     { input }: gql.MutationCreateImageCommentArgs,
     context: Context,
-  ) => {
+  ): Promise<gql.ImageCommentsPayload> => {
     const res = await context.models.Image.createComment(input, context);
     return { ...res };
   },
 
-  createImage: async (_: unknown, { input }: gql.MutationCreateImageArgs, context: Context) => {
+  createImage: async (
+    _: unknown,
+    { input }: gql.MutationCreateImageArgs,
+    context: Context,
+  ): Promise<gql.CreateImagePayload> => {
     const imageAttempt = await context.models.Image.createImage(input, context);
     return { imageAttempt };
   },
 
-  deleteImages: async (_: unknown, { input }: gql.MutationDeleteImagesArgs, context: Context) => {
+  deleteImages: async (
+    _: unknown,
+    { input }: gql.MutationDeleteImagesArgs,
+    context: Context,
+  ): Promise<gql.StandardErrorPayload> => {
     const res = await context.models.Image.deleteImages(input, context);
     return { ...res };
   },
@@ -114,7 +150,7 @@ export default {
     _: unknown,
     { input }: gql.MutationRegisterCameraArgs,
     context: Context,
-  ) => {
+  ): Promise<gql.RegisterCameraPayload> => {
     const res = await context.models.Camera.registerCamera(input, context);
     return { ...res };
   },
@@ -123,17 +159,25 @@ export default {
     _: unknown,
     { input }: gql.MutationUnregisterCameraArgs,
     context: Context,
-  ) => {
+  ): Promise<gql.UnregisterCameraPayload> => {
     const res = await context.models.Camera.unregisterCamera(input, context);
     return { ...res };
   },
 
-  createProject: async (_: unknown, { input }: gql.MutationCreateProjectArgs, context: Context) => {
+  createProject: async (
+    _: unknown,
+    { input }: gql.MutationCreateProjectArgs,
+    context: Context,
+  ): Promise<gql.ProjectPayload> => {
     const project = await context.models.Project.createProject(input, context);
     return { project };
   },
 
-  updateProject: async (_: unknown, { input }: gql.MutationUpdateProjectArgs, context: Context) => {
+  updateProject: async (
+    _: unknown,
+    { input }: gql.MutationUpdateProjectArgs,
+    context: Context,
+  ): Promise<gql.ProjectPayload> => {
     const project = await context.models.Project.updateProject(input, context);
     return { project };
   },
@@ -142,7 +186,7 @@ export default {
     _: unknown,
     { input }: gql.MutationCreateProjectLabelArgs,
     context: Context,
-  ) => {
+  ): Promise<gql.ProjectLabelPayload> => {
     const label = await context.models.Project.createLabel(input, context);
     return { label };
   },
@@ -151,7 +195,7 @@ export default {
     _: unknown,
     { input }: gql.MutationUpdateProjectLabelArgs,
     context: Context,
-  ) => {
+  ): Promise<gql.ProjectLabelPayload> => {
     const label = await context.models.Project.updateLabel(input, context);
     return { label };
   },
@@ -160,22 +204,34 @@ export default {
     _: unknown,
     { input }: gql.MutationDeleteProjectLabelArgs,
     context: Context,
-  ) => {
+  ): Promise<gql.StandardPayload> => {
     const res = await context.models.Project.deleteLabel(input, context);
     return { ...res };
   },
 
-  createView: async (_: unknown, { input }: gql.MutationCreateViewArgs, context: Context) => {
+  createView: async (
+    _: unknown,
+    { input }: gql.MutationCreateViewArgs,
+    context: Context,
+  ): Promise<gql.CreateViewPayload> => {
     const view = await context.models.Project.createView(input, context);
     return { view };
   },
 
-  updateView: async (_: unknown, { input }: gql.MutationUpdateViewArgs, context: Context) => {
+  updateView: async (
+    _: unknown,
+    { input }: gql.MutationUpdateViewArgs,
+    context: Context,
+  ): Promise<gql.UpdateViewPayload> => {
     const view = await context.models.Project.updateView(input, context);
     return { view };
   },
 
-  deleteView: async (_: unknown, { input }: gql.MutationDeleteViewArgs, context: Context) => {
+  deleteView: async (
+    _: unknown,
+    { input }: gql.MutationDeleteViewArgs,
+    context: Context,
+  ): Promise<gql.DeleteViewPayload> => {
     const project = await context.models.Project.deleteView(input, context);
     return { project };
   },
@@ -184,7 +240,7 @@ export default {
     _: unknown,
     { input }: gql.MutationUpdateAutomationRulesArgs,
     context: Context,
-  ) => {
+  ): Promise<gql.UpdateAutomationRulesPayload> => {
     const automationRules = await context.models.Project.updateAutomationRules(input, context);
     return { automationRules };
   },
@@ -193,37 +249,49 @@ export default {
     _: unknown,
     { input }: gql.MutationCreateDeploymentArgs,
     context: Context,
-  ) => {
-    return await context.models.Project.createDeployment(input, context);
+  ): Promise<gql.Task> => {
+    return context.models.Project.createDeployment(input, context);
   },
 
   updateDeployment: async (
     _: unknown,
     { input }: gql.MutationUpdateDeploymentArgs,
     context: Context,
-  ) => {
-    return await context.models.Project.updateDeployment(input, context);
+  ): Promise<gql.Task> => {
+    return context.models.Project.updateDeployment(input, context);
   },
 
   deleteDeployment: async (
     _: unknown,
     { input }: gql.MutationDeleteDeploymentArgs,
     context: Context,
-  ) => {
-    return await context.models.Project.deleteDeployment(input, context);
+  ): Promise<gql.Task> => {
+    return context.models.Project.deleteDeployment(input, context);
   },
 
-  createObjects: async (_: unknown, { input }: gql.MutationCreateObjectsArgs, context: Context) => {
+  createObjects: async (
+    _: unknown,
+    { input }: gql.MutationCreateObjectsArgs,
+    context: Context,
+  ): Promise<gql.StandardPayload> => {
     const res = await context.models.Image.createObjects(input, context);
     return { isOk: res.ok };
   },
 
-  updateObjects: async (_: unknown, { input }: gql.MutationUpdateObjectsArgs, context: Context) => {
+  updateObjects: async (
+    _: unknown,
+    { input }: gql.MutationUpdateObjectsArgs,
+    context: Context,
+  ): Promise<gql.StandardPayload> => {
     const res = await context.models.Image.updateObjects(input, context);
     return { isOk: res.ok };
   },
 
-  deleteObjects: async (_: unknown, { input }: gql.MutationDeleteObjectsArgs, context: Context) => {
+  deleteObjects: async (
+    _: unknown,
+    { input }: gql.MutationDeleteObjectsArgs,
+    context: Context,
+  ): Promise<gql.StandardPayload> => {
     const res = await context.models.Image.deleteObjects(input, context);
     return { isOk: res.ok };
   },
@@ -232,22 +300,36 @@ export default {
     _: unknown,
     { input }: gql.MutationCreateInternalLabelsArgs,
     context: Context,
-  ) => {
+  ): Promise<gql.StandardPayload> => {
     const res = await context.models.Image.createInternalLabels(input, context);
-    return { isOk: res.ok };
+    return {
+      isOk: res.ok,
+    };
   },
 
-  createLabels: async (_: unknown, { input }: gql.MutationCreateLabelsArgs, context: Context) => {
+  createLabels: async (
+    _: unknown,
+    { input }: gql.MutationCreateLabelsArgs,
+    context: Context,
+  ): Promise<gql.StandardPayload> => {
     const res = await context.models.Image.createLabels(input, context);
     return { isOk: res.ok };
   },
 
-  updateLabels: async (_: unknown, { input }: gql.MutationUpdateLabelsArgs, context: Context) => {
+  updateLabels: async (
+    _: unknown,
+    { input }: gql.MutationUpdateLabelsArgs,
+    context: Context,
+  ): Promise<gql.StandardPayload> => {
     const res = await context.models.Image.updateLabels(input, context);
     return { isOk: res.ok };
   },
 
-  deleteLabels: async (_: unknown, { input }: gql.MutationDeleteLabelsArgs, context: Context) => {
+  deleteLabels: async (
+    _: unknown,
+    { input }: gql.MutationDeleteLabelsArgs,
+    context: Context,
+  ): Promise<gql.StandardPayload> => {
     const res = await context.models.Image.deleteLabels(input, context);
     return { isOk: res.ok };
   },
--- src/api/resolvers/Query.ts
+++ src/api/resolvers/Query.ts
@@ -2,15 +2,27 @@ import type * as gql from '../../@types/graphql.js';
 import { Context } from '../handler.js';
 
 export default {
-  projects: async (_: unknown, { input }: gql.QueryProjectsArgs, context: Context) => {
-    return await context.models.Project.getProjects(input, context);
+  projects: async (
+    _: unknown,
+    { input }: gql.QueryProjectsArgs,
+    context: Context,
+  ): Promise<gql.Project[]> => {
+    return context.models.Project.getProjects(input, context);
   },
 
-  users: async (_: unknown, { input }: gql.QueryUsersArgs, context: Context) => {
-    return await context.models.User.listUsers(input, context);
+  users: async (
+    _: unknown,
+    { input }: gql.QueryUsersArgs,
+    context: Context,
+  ): Promise<gql.UsersPayload> => {
+    return context.models.User.listUsers(input, context);
   },
 
-  tasks: async (_: unknown, { input }: gql.QueryTasksArgs, context: Context) => {
+  tasks: async (
+    _: unknown,
+    { input }: gql.QueryTasksArgs,
+    context: Context,
+  ): Promise<gql.TasksPayload> => {
     const response = await context.models.Task.queryByFilter(input, context);
     const { previous, hasPrevious, next, hasNext, results } = response;
     return {
@@ -24,11 +36,15 @@ export default {
     };
   },
 
-  task: async (_: unknown, { input }: gql.QueryTaskArgs, context: Context) => {
-    return await context.models.Task.queryById(input.taskId, context);
+  task: async (_: unknown, { input }: gql.QueryTaskArgs, context: Context): Promise<gql.Task> => {
+    return context.models.Task.queryById(input.taskId, context);
   },
 
-  batches: async (_: unknown, { input }: gql.QueryBatchesArgs, context: Context) => {
+  batches: async (
+    _: unknown,
+    { input }: gql.QueryBatchesArgs,
+    context: Context,
+  ): Promise<gql.BatchesConnection> => {
     const response = await context.models.Batch.queryByFilter(input, context);
     const { previous, hasPrevious, next, hasNext, results } = response;
     return {
@@ -42,7 +58,11 @@ export default {
     };
   },
 
-  images: async (_: unknown, { input }: gql.QueryImagesArgs, context: Context) => {
+  images: async (
+    _: unknown,
+    { input }: gql.QueryImagesArgs,
+    context: Context,
+  ): Promise<gql.ImagesConnection> => {
     console.time('images query');
     const response = await context.models.Image.queryByFilter(input, context);
     console.timeEnd('images query');
@@ -58,7 +78,11 @@ export default {
     };
   },
 
-  imagesCount: async (_: unknown, { input }: gql.QueryImagesCountArgs, context: Context) => {
+  imagesCount: async (
+    _: unknown,
+    { input }: gql.QueryImagesCountArgs,
+    context: Context,
+  ): Promise<gql.ImagesCount> => {
     console.log('imagesCount query - input: ', input);
     console.time('images count');
     const count = await context.models.Image.countImages(input, context);
@@ -68,7 +92,11 @@ export default {
     };
   },
 
-  imageErrors: async (_: unknown, { input }: gql.QueryImageErrorsArgs, context: Context) => {
+  imageErrors: async (
+    _: unknown,
+    { input }: gql.QueryImageErrorsArgs,
+    context: Context,
+  ): Promise<gql.ImageErrorsConnection> => {
     const count = await context.models.ImageError.countImageErrors(input);
     const response = await context.models.ImageError.queryByFilter(input);
     const { previous, hasPrevious, next, hasNext, results } = response;
@@ -84,35 +112,47 @@ export default {
     };
   },
 
-  image: async (_: unknown, { input }: gql.QueryImageArgs, context: Context) => {
-    return await context.models.Image.queryById(input.imageId, context);
+  image: async (
+    _: unknown,
+    { input }: gql.QueryImageArgs,
+    context: Context,
+  ): Promise<gql.Image> => {
+    return context.models.Image.queryById(input.imageId, context);
   },
 
   wirelessCameras: async (
     _: unknown,
     { input }: gql.QueryWirelessCamerasArgs,
     context: Context,
-  ) => {
-    return await context.models.Camera.getWirelessCameras(input, context);
+  ): Promise<gql.WirelessCamera[]> => {
+    return context.models.Camera.getWirelessCameras(input, context);
   },
 
-  mlModels: async (_: unknown, { input }: gql.QueryMlModelsArgs, context: Context) => {
-    return await context.models.MLModel.getMLModels(input);
+  mlModels: async (
+    _: unknown,
+    { input }: gql.QueryMlModelsArgs,
+    context: Context,
+  ): Promise<gql.MlModel[]> => {
+    return context.models.MLModel.getMLModels(input);
   },
 
-  stats: async (_: unknown, { input }: gql.QueryStatsArgs, context: Context) => {
-    return await context.models.Image.getStats(input, context);
+  stats: async (_: unknown, { input }: gql.QueryStatsArgs, context: Context): Promise<gql.Task> => {
+    return context.models.Image.getStats(input, context);
   },
 
-  exportErrors: async (_: unknown, { input }: gql.QueryExportErrorsArgs, context: Context) => {
-    return await context.models.ImageError.exportErrors(input, context);
+  exportErrors: async (
+    _: unknown,
+    { input }: gql.QueryExportErrorsArgs,
+    context: Context,
+  ): Promise<gql.Task> => {
+    return context.models.ImageError.exportErrors(input, context);
   },
 
   exportAnnotations: async (
     _: unknown,
     { input }: gql.QueryExportAnnotationsArgs,
     context: Context,
-  ) => {
-    return await context.models.Image.exportAnnotations(input, context);
+  ): Promise<gql.Task> => {
+    return context.models.Image.exportAnnotations(input, context);
   },
 };
