26c78283e0173ba556b5b754fa6413f22e685218
Anthony Lukach <anthonylukach@gmail.com>
2024-06-11 17:51:09 -0700

    Add types for mongo-cursor-pagination

--- src/@types/mongo-cursor-pagination.d.ts
+++ src/@types/mongo-cursor-pagination.d.ts
@@ -1,14 +1,17 @@
 declare module 'mongo-cursor-pagination' {
   import { Collection } from 'mongoose';
 
-  export type AggregationInput<T = {}> = T & {
-    paginatedField: string;
-    sortAscending: boolean;
-    limit: number;
-    next: string;
-    previous: string;
+  export type AggregationInput = {
+    aggregation: Array<{
+      $match: Record<string, any>;
+      $set?: Record<string, any>;
+    }>;
+    paginatedField?: string;
+    sortAscending?: boolean;
+    limit?: number;
+    next?: string;
+    previous?: string;
   };
-
   export interface AggregationOutput<T> {
     metadata: {
       total: number;
