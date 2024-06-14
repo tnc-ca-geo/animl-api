bcf3030864c8ac4e2743ff37a181cfd9e05d2b2a
Anthony Lukach <anthonylukach@gmail.com>
2024-06-14 09:44:16 -0700

    Update after rebase

--- src/api/db/models/BatchError.ts
+++ src/api/db/models/BatchError.ts
@@ -2,7 +2,7 @@ import GraphQLError, { InternalServerError } from '../../errors.js';
 import { WRITE_IMAGES_ROLES } from '../../auth/roles.js';
 import BatchError, { BatchErrorSchema } from '../schemas/BatchError.js';
 import retry from 'async-retry';
-import { BaseAuthedModel, GenericOutput, MethodParams, roleCheck } from './utils-model.js';
+import { BaseAuthedModel, GenericResponse, MethodParams, roleCheck } from './utils-model.js';
 
 /**
  * BatchErrors are errors that are generated when an uploaded Zip fails
@@ -42,7 +42,7 @@ export class BatchErrorModel {
    * @param {Object} input
    * @param {String} input.batch
    */
-  static async clearErrors(input: { batch: string }): Promise<GenericOutput> {
+  static async clearErrors(input: { batch: string }): Promise<GenericResponse> {
     try {
       await retry(
         () =>
