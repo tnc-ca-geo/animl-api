40cc2fd60a2316b59f63bc33a530dfbd08e03bb4
Anthony Lukach <anthonylukach@gmail.com>
2024-06-18 15:13:53 -0700

    Convert tasks model

--- src/api/db/models/Task.ts
+++ src/api/db/models/Task.ts
@@ -1,11 +1,13 @@
 import { NotFoundError, ForbiddenError, AuthenticationError } from '../../errors.js';
 import SQS from '@aws-sdk/client-sqs';
 import MongoPaging from 'mongo-cursor-pagination';
-import Task from '../schemas/Task.js';
+import Task, { TaskSchema } from '../schemas/Task.js';
 import { hasRole } from './utils.js';
-import {
-  READ_TASKS_ROLES
-} from '../../auth/roles.js';
+import { READ_TASKS_ROLES } from '../../auth/roles.js';
+import { BaseAuthedModel, MethodParams, roleCheck } from './utils-model.js';
+import { Context } from '../../handler.js';
+import * as gql from '../../../@types/graphql.js';
+import { HydratedDocument } from 'mongoose';
 
 /**
  * Tasks manage the state of async events (except for batch uploads) on the platform
@@ -23,21 +25,21 @@ export class TaskModel {
    * @param {String} input.previous
    * @param {Object} context
    */
-  static async queryByFilter(input, context) {
+  static async queryByFilter(input: gql.QueryTasksInput, context: Context) {
     return await MongoPaging.aggregate(Task.collection, {
       aggregation: [
-        { '$match': { 'projectId': context.user['curr_project'] } },
-        { '$match': { 'user': context.user.sub } }
+        { $match: { projectId: context.user['curr_project'] } },
+        { $match: { user: context.user.sub } },
       ],
       limit: input.limit,
       paginatedField: input.paginatedField,
       sortAscending: input.sortAscending,
       next: input.next,
-      previous: input.previous
+      previous: input.previous,
     });
   }
 
-  static async queryById(_id, context) {
+  static async queryById(_id: string, context: Context) {
     const query = { _id };
     const task = await Task.findOne(query);
     if (!task) throw new NotFoundError('Task not found');
@@ -49,28 +51,33 @@ export class TaskModel {
     return task;
   }
 
-  static async create(input, context) {
+  static async create<T>(input: TaskInput<T>, context: Context) {
     const task = new Task({
       user: input.user,
       projectId: input.projectId,
-      type: input.type
+      type: input.type,
     });
 
     const sqs = new SQS.SQSClient({ region: process.env.AWS_DEFAULT_REGION });
 
     await task.save();
 
-    await sqs.send(new SQS.SendMessageCommand({
-      QueueUrl: context.config['/TASKS/TASK_QUEUE_URL'],
-      MessageBody: JSON.stringify({
-        config: input.config,
-        ...task.toJSON()
-      })
-    }));
+    await sqs.send(
+      new SQS.SendMessageCommand({
+        QueueUrl: context.config['/TASKS/TASK_QUEUE_URL'],
+        MessageBody: JSON.stringify({
+          config: input.config,
+          ...task.toJSON(),
+        }),
+      }),
+    );
     return task;
   }
 
-  static async update(input, context) {
+  static async update(
+    input: Partial<TaskSchema> & { _id: gql.Scalars['ID']['input'] },
+    context: Context,
+  ): Promise<HydratedDocument<TaskSchema>> {
     const task = await this.queryById(input._id, context);
 
     input.updated = new Date();
@@ -81,21 +88,18 @@ export class TaskModel {
   }
 }
 
-export default class AuthedTaskModel {
-  constructor(user) {
-    if (!user) throw new AuthenticationError('Authentication failed');
-    this.user = user;
+export default class AuthedTaskModel extends BaseAuthedModel {
+  @roleCheck(READ_TASKS_ROLES)
+  queryById(...args: MethodParams<typeof TaskModel.queryById>) {
+    return TaskModel.queryById(...args);
   }
 
-  async queryById(input, context) {
-    if (!hasRole(this.user, READ_TASKS_ROLES)) throw new ForbiddenError();
-
-    return await TaskModel.queryById(input, context);
+  @roleCheck(READ_TASKS_ROLES)
+  queryByFilter(...args: MethodParams<typeof TaskModel.queryByFilter>) {
+    return TaskModel.queryByFilter(...args);
   }
+}
 
-  async queryByFilter(input, context) {
-    if (!hasRole(this.user, READ_TASKS_ROLES)) throw new ForbiddenError();
-
-    return await TaskModel.queryByFilter(input, context);
-  }
+export interface TaskInput<T> extends Pick<TaskSchema, 'user' | 'projectId' | 'type'> {
+  config: T;
 }
