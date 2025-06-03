b2967011764c6f6be4792a99646f8ba96240acc2
Anthony Lukach <anthonylukach@gmail.com>
2025-06-03 16:00:23 -0700

    In progress

--- src/api/db/schemas/Image.ts
+++ src/api/db/schemas/Image.ts
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose';
 import MongoPaging from 'mongo-cursor-pagination';
 import { LocationSchema, ObjectSchema } from './shared/index.js';
-import { randomUUID } from 'node:crypto';
+import { buildImgUrl, signUrl } from '../models/utils.js';
+import { getConfig } from '../../../config/config.js';
 
 const Schema = mongoose.Schema;
 
@@ -18,33 +19,50 @@ const ImageCommentSchema = new Schema({
   comment: { type: String, required: true },
 });
 
-const ImageSchema = new Schema({
-  _id: { type: String, required: true },
-  bucket: { type: String, required: true },
-  batchId: { type: String },
-  fileTypeExtension: { type: String, required: true },
-  path: { type: String },
-  dateAdded: { type: Date, default: Date.now, required: true },
-  dateTimeOriginal: { type: Date, required: true },
-  timezone: { type: String, required: true },
-  make: { type: String, default: 'unknown', required: true },
-  cameraId: { type: String, required: true, ref: 'Camera' },
-  deploymentId: { type: Schema.Types.ObjectId, ref: 'Deployment', required: true },
-  projectId: { type: String, required: true, ref: 'Project' },
-  originalFileName: { type: String },
-  imageWidth: { type: Number },
-  imageHeight: { type: Number },
-  imageBytes: { type: Number },
-  mimeType: { type: String },
-  userSetData: { type: Map, of: String },
-  model: { type: String },
-  location: { type: LocationSchema },
-  triggerSource: { type: String },
-  reviewed: { type: Boolean },
-  objects: { type: [ObjectSchema] },
-  comments: { type: [ImageCommentSchema] },
-  tags: { type: [mongoose.Schema.Types.ObjectId] },
-});
+const ImageSchema = new Schema(
+  {
+    _id: { type: String, required: true },
+    bucket: { type: String, required: true },
+    batchId: { type: String },
+    fileTypeExtension: { type: String, required: true },
+    path: { type: String },
+    dateAdded: { type: Date, default: Date.now, required: true },
+    dateTimeOriginal: { type: Date, required: true },
+    timezone: { type: String, required: true },
+    make: { type: String, default: 'unknown', required: true },
+    cameraId: { type: String, required: true, ref: 'Camera' },
+    deploymentId: { type: Schema.Types.ObjectId, ref: 'Deployment', required: true },
+    projectId: { type: String, required: true, ref: 'Project' },
+    originalFileName: { type: String },
+    imageWidth: { type: Number },
+    imageHeight: { type: Number },
+    imageBytes: { type: Number },
+    mimeType: { type: String },
+    userSetData: { type: Map, of: String },
+    model: { type: String },
+    location: { type: LocationSchema },
+    triggerSource: { type: String },
+    reviewed: { type: Boolean },
+    objects: { type: [ObjectSchema] },
+    comments: { type: [ImageCommentSchema] },
+    tags: { type: [mongoose.Schema.Types.ObjectId] },
+  },
+  {
+    virtuals: {
+      signedImageUrl: {
+        // TODO: Verify that this works async...
+        async get(): Promise<{ url: string; thumbUrl: string }> {
+          const config = await getConfig();
+          const privateKey = config[`/IMAGES/CLOUDFRONT_DISTRIBUTION_PRIVATEKEY`];
+          return {
+            url: signUrl(buildImgUrl(this, config, 'original'), privateKey),
+            thumbUrl: signUrl(buildImgUrl(this, config, 'small'), privateKey),
+          };
+        },
+      },
+    },
+  },
+);
 
 ImageSchema.plugin(MongoPaging.mongoosePlugin);
 
--- src/config/config.ts
+++ src/config/config.ts
@@ -44,6 +44,7 @@ const ssmNames = [
   `/frontend/url-${process.env.STAGE}`,
   `/api/url-${process.env.STAGE}`,
   `/images/url-${process.env.STAGE}`,
+  `/images/cloudfront-distribution-privatekey-${process.env.STAGE}`,
   `/ml/inference-queue-url-${process.env.STAGE}`,
   `/exports/exported-data-bucket-${process.env.STAGE}`,
   `/tasks/task-queue-url-${process.env.STAGE}`,
@@ -131,6 +132,7 @@ export interface RemoteConfig {
   '/EXPORTS/EXPORTED_DATA_BUCKET': string;
   '/FRONTEND/URL': string;
   '/IMAGES/URL': string;
+  '/IMAGES/CLOUDFRONT_DISTRIBUTION_PRIVATEKEY': string;
   '/ML/INFERENCE_QUEUE_URL': string;
   '/ML/MEGADETECTOR_V5A_BATCH_ENDPOINT': string;
   '/ML/MEGADETECTOR_V5A_REALTIME_ENDPOINT': string;
