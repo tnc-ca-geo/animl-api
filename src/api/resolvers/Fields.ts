d0f4852d65708e78ce7ac25a7a43fcf6c4dd6df9
Nathaniel Rindlaub <nathanielrindlaub@gmail.com>
2025-07-02 16:59:18 -0700

    Add comment about field level resolvers

--- src/api/resolvers/Fields.ts
+++ src/api/resolvers/Fields.ts
@@ -2,6 +2,10 @@ import { Context } from '../handler.js';
 import type { ImageSchema } from '../db/schemas/Image.js';
 import { buildImgUrl } from '../db/models/utils.js';
 
+// Field level resolvers
+// NOTE: For more information on resolver chains and when to use field-level resolvers, see:
+// https://www.apollographql.com/docs/apollo-server/data/resolvers/#resolver-chains
+
 export default {
   Image: {
     url: (parent: ImageSchema, _: unknown, context: Context) =>
