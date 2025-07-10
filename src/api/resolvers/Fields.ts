46748120d154d5d255f54dfd183182d483e7c1d6
Anthony Lukach <anthonylukach@gmail.com>
2025-07-10 16:28:42 -0700

    Improve typing around buildImgUrl
    * Fix alert image TTL by setting to 50 years (was passing in integer in unit of minutes rather than years)
    * Add `Duration` type
    * Make buildImgUrl default to 2 weeks
    * Add typing for size

--- src/api/resolvers/Fields.ts
+++ src/api/resolvers/Fields.ts
@@ -1,6 +1,7 @@
 import { Context } from '../handler.js';
 import type { ImageSchema } from '../db/schemas/Image.js';
 import { buildImgUrl } from '../db/models/utils.js';
+import type { SignedImageUrl } from '../../@types/graphql.js';
 
 // Field level resolvers
 // NOTE: For more information on resolver chains and when to use field-level resolvers, see:
@@ -10,7 +11,7 @@ export default {
   Image: {
     url: (parent: ImageSchema, _: unknown, context: Context) =>
       Object.fromEntries(
-        ['original', 'medium', 'small'].map((size) => [
+        (['original', 'medium', 'small'] as Array<keyof SignedImageUrl>).map((size) => [
           size,
           buildImgUrl(parent, context.config, size),
         ]),
