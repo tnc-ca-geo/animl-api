31e3aeb40c1eeda30df6a07cda4ca6753e24a8a8
Oliver Roick <oliver@roick.email>
2025-07-14 16:41:53 +1000

    Rename sequence to burst

--- src/task/getBursts.ts
+++ src/task/getBursts.ts
@@ -10,7 +10,7 @@ import { AggregationLevel } from '../@types/graphql.js';
 import Image, { ImageSchema } from '../api/db/schemas/Image.js';
 
 
-const MAX_SEQUENCE_DELTA = 2;
+const MAX_BURST_DELTA = 2;
 
 type Task = TaskInput<{ filters: FiltersSchema, aggregationLevel: AggregationLevel }>
 export type BurstsTask = TaskInput<{ filters: FiltersSchema, aggregationLevel: AggregationLevel.burst }>;
@@ -38,25 +38,25 @@ export default async function getBurstStats(task: Task): Promise<GetBurstOutput>
   let burstCount = 0;
   const burstLabelList: Record<string, number> = {};
 
-  const processBurst = (sequence: ImageSchema[]) => {
+  const processBurst = (burst: ImageSchema[]) => {
     burstCount++;
 
-    const sequenceLabels: string[] = [];
+    const burstLabels: string[] = [];
 
-    for (const img of sequence) {
+    for (const img of burst) {
       for (const obj of img.objects) {
         const representativeLabel = findRepresentativeLabel(obj);
         if (representativeLabel) {
           const projLabel = project.labels.find((lbl) => idMatch(lbl._id, representativeLabel.labelId));
           const labelName = projLabel?.name || 'ERROR FINDING LABEL';
-          if (!sequenceLabels.includes(labelName)) {
-            sequenceLabels.push(labelName);
+          if (!burstLabels.includes(labelName)) {
+            burstLabels.push(labelName);
           }
         }
       }
     }
 
-    for (const label of sequenceLabels) {
+    for (const label of burstLabels) {
       burstLabelList[label] = Object.prototype.hasOwnProperty.call(burstLabelList, label)
         ? burstLabelList[label] + 1
         : 1;
@@ -70,33 +70,34 @@ export default async function getBurstStats(task: Task): Promise<GetBurstOutput>
         deploymentId: dep._id,
       }
     });
+    depPipeline.push({ $sort: { dateTimeOriginal: 1 } });
 
-    let sequence: ImageSchema[] = [];
+    let burst: ImageSchema[] = [];
 
     for await (const img of Image.aggregate(depPipeline)) {
-      if (sequence.length === 0) {
-        sequence.push(img);
+      if (burst.length === 0) {
+        burst.push(img);
         continue;
       }
 
-      const lastImg = sequence[sequence.length - 1];
+      const lastImg = burst[burst.length - 1];
       const imgDateAdded = DateTime.fromJSDate(img.dateTimeOriginal);
       const lastImgDateAdded = DateTime.fromJSDate(lastImg.dateTimeOriginal);
       const diff = lastImgDateAdded.diff(imgDateAdded, 'seconds').toObject();
       const delta = Math.abs(diff.seconds || 0);
 
-      // if the delta between the last image and the current image is less than the max sequence delta,
-      if (delta <= MAX_SEQUENCE_DELTA) {
-        // image belongs to current sequence
-        sequence.push(img);
+      // if the delta between the last image and the current image is less than the max burst delta,
+      if (delta <= MAX_BURST_DELTA) {
+        // image belongs to current burst
+        burst.push(img);
       } else {
         // found a gap,
-        processBurst(sequence);
-        sequence = [img];
+        processBurst(burst);
+        burst = [img];
       }
     }
-    if (sequence.length > 0) {
-      processBurst(sequence);
+    if (burst.length > 0) {
+      processBurst(burst);
     }
   }
 
