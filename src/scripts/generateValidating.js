14456242bf8e96aaf477312e506a2c1029ca40bd
Jesse Leung <jessetleung@gmail.com>
2025-09-07 17:37:10 -0400

    feat: output to file, use validating labels

--- src/scripts/generateValidating.js
+++ src/scripts/generateValidating.js
@@ -2,141 +2,126 @@ import { getConfig } from '../../.build/config/config.js';
 import { connectToDatabase } from '../../.build/api/db/connect.js';
 import Image from '../../.build/api/db/schemas/Image.js';
 import Project from '../../.build/api/db/schemas/Project.js';
+import MLModel from '../../.build/api/db/schemas/MLModel.js';
+import cliProgress from 'cli-progress';
+import fs from 'node:fs';
+import util from 'util';
+import path from 'node:path';
+import appRoot from 'app-root-path';
 
-const generateValidationListConfig = {
-  PROJECT_ID: 'sci_biosecurity',
-  START_DATE: '2023-4-28',
-  END_DATE: '2024-5-29',
-  SKIP_MODELS: ['megadetector'],
-  SKIP_EMPTY: true
-};
+async function getCount(pipeline) {
+  console.log('getting image count');
+  let count = null;
+  try {
+    const pipelineCopy = structuredClone(pipeline);
+    pipelineCopy.push({ $count: 'count' });
+    const res = await Image.aggregate(pipelineCopy);
+    count = res[0] ? res[0].count : 0;
+  } catch (err) {
+    console.log('error counting Image: ', err);
+  }
+  return count;
+}
 
+const buildBasePipeline = (projectId) => [
+  {
+    $match: {
+      projectId: projectId,
+      reviewed: true,
+      'objects.labels.validation.validated': true,
+    },
+  },
+  {
+    $set: {
+      objects: {
+        $map: {
+          input: '$objects',
+          as: 'obj',
+          in: {
+            $setField: {
+              field: 'firstValidLabel',
+              input: '$$obj',
+              value: {
+                $filter: {
+                  input: '$$obj.labels',
+                  as: 'label',
+                  cond: {
+                    $eq: ['$$label.validation.validated', true],
+                  },
+                  limit: 1,
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+];
+
+// This generates the predicted and validating TARGET_CLASS list
+// for analysis config.
+//
+// 1. Get ML model categories for target model
+// 2. Map categories to project labels by name
+// 3. Get all images in project which have a label
+// from the target ML model
+// 4. For the first ML model label found, add the
+// representative label to its validating list
 export const generateValidationList = async (genConfig) => {
   const config = await getConfig();
   console.log('Connecting to db...');
   const dbClient = await connectToDatabase(config);
 
   try {
-    const { PROJECT_ID, START_DATE, END_DATE, SKIP_MODELS, SKIP_EMPTY } = genConfig;
+    const { PROJECT_ID, MODEL } = genConfig;
 
-    const projectDbTime = performance.now();
     const project = await Project.findOne({
       _id: PROJECT_ID
     });
-    console.log(`Project db query time: ${(performance.now() - projectDbTime) / 1000} seconds`);
-
     const projectLabels = project.labels.reduce((acc, lbl) => {
       acc[lbl._id] = lbl.name;
       return acc;
     }, {});
-    const projectLabelIds = Object.keys(projectLabels);
 
-    // Prepare map { labelId: [validatingLabelId] }
-    const validatingLabels = projectLabelIds.reduce((acc, lbl) => {
-      return { ...acc, [lbl]: [] };
+    const model = await MLModel.findOne({
+      _id: MODEL
+    });
+    const modelLabelIds = model.categories.map((category) => {
+      const projectLabel = project.labels.find((lbl) => lbl.name === category.name);
+      return projectLabel._id;
+    });
+
+    const validationLabels = modelLabelIds.reduce((acc, lblId) => {
+      acc[lblId] = new Set();
+      return acc;
     }, {});
 
-    console.log('Collecting images...');
-    const imageDbTimer = performance.now();
+    const baseImagePipeline = buildBasePipeline(PROJECT_ID);
+    const imageCount = await getCount(baseImagePipeline);
 
-    const images = await Image.aggregate([
-      {
-        $match: {
-          projectId: PROJECT_ID,
-          dateAdded: {
-            $gte: new Date(START_DATE),
-            $lte: new Date(END_DATE),
-          },
-          reviewed: true,
-          'objects.labels.labelId': {
-            $in: projectLabelIds
-          },
-          'objects.labels.validation.validated': true,
-        },
-      },
-      {
-        $set: {
-          objects: {
-            $map: {
-              input: '$objects',
-              as: 'obj',
-              in: {
-                $setField: {
-                  field: 'firstValidLabel',
-                  input: '$$obj',
-                  value: {
-                    $filter: {
-                      input: '$$obj.labels',
-                      as: 'label',
-                      cond: {
-                        $eq: ['$$label.validation.validated', true],
-                      },
-                      limit: 1,
-                    },
-                  },
-                },
-              },
-            },
-          },
-        },
-      },
-      {
-        $set: {
-          objects: {
-            $map: {
-              input: '$objects',
-              as: 'obj',
-              in: {
-                $setField: {
-                  field: 'filteredLabels',
-                  input: '$$obj',
-                  value: {
-                    $filter: {
-                      input: '$$obj.labels',
-                      as: 'label',
-                      cond: {
-                        $and: [
-                          { $ne: ['$$obj.firstValidLabel.labelId', null] },
-                          { $eq: ['$$label.validation.validated', true] },
-                          { $ne: ['$$label.mlModel', { $in: ['$$label.mlModel', SKIP_MODELS] }] },
-                          ...( SKIP_EMPTY && [{ $ne: ['$$label.labelId', 'empty'] }])
-                        ]
-                      },
-                    },
-                  },
-                },
-              },
-            },
-          },
-        },
-      },
-    ]);
+    const progress = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
 
-    console.log(`Image db query time: ${(performance.now() - imageDbTimer) / 1000} seconds`);
+    progress.start(imageCount, 0);
+    for await (const img of Image.aggregate(baseImagePipeline)) {
+      for (const obj of img.objects) {
+        const firstValidated = obj.firstValidLabel.shift();
+        const predicted = obj.labels.find((lbl) => lbl.mlModel === MODEL);
 
-    console.log('Building validation lists...');
-    const processTimer = performance.now();
-
-    const validationLists = images.reduce((imgAcc, img) => {
-      img.objects.forEach((obj) => {
-        if (!obj.firstValidLabel || obj.firstValidLabel.length < 1) {
-          return;
+        if (!firstValidated || !predicted) {
+          continue;
         }
 
-        const labelIds = obj.filteredLabels.map((lbl) => lbl.labelId);
-        const firstValidLabelId = obj.firstValidLabel.shift().labelId;
-        const curr = validatingLabels[firstValidLabelId];
-        validatingLabels[firstValidLabelId] = new Set([...curr, ...labelIds]);
-      });
-      return imgAcc;
-    }, validatingLabels);
+        validationLabels[predicted.labelId].add(firstValidated.labelId);
+      }
 
-    console.log(`Validation list generation time: ${(performance.now() - processTimer) / 1000} seconds`);
+      progress.increment();
+    }
+    progress.stop();
 
     console.log('Mapping label IDs to label names...');
-    const nameTimer = performance.now();
 
-    const validationListsWithNames = Object.entries(validationLists).map(([labelId, validations]) => {
+    const validationListsWithNames = Object.entries(validationLabels).map(([labelId, validations]) => {
       const labelName = projectLabels[labelId];
 
       const validationNames = Array.from(validations).map((validation) => {
@@ -144,11 +129,10 @@ export const generateValidationList = async (genConfig) => {
       });
 
       return {
-        [`${labelName}:${labelId}`]: validationNames
+        predicted: `${labelName}:${labelId}`,
+        validation: validationNames,
       };
     });
-
-    console.log(`Label name mapping time: ${(performance.now() - nameTimer) / 1000} seconds`);
 
     return validationListsWithNames;
   } catch (err) {
@@ -159,12 +143,34 @@ export const generateValidationList = async (genConfig) => {
   }
 };
 
-// Example
-const startTimer = performance.now();
+const writeToFile = (config, output) => {
+  const { ANALYSIS_DIR } = config;
+  const analysisPath = path.join(appRoot.path, ANALYSIS_DIR);
+  const outputFilename = path.join(analysisPath, 'GENERATED_TARGET_CLASSES.js');
 
+  if (!fs.existsSync(analysisPath)) {
+    fs.mkdirSync(analysisPath, { recursive: true });
+  }
+
+  try {
+    fs.writeFileSync(outputFilename, output, 'utf8');
+  } catch (err) {
+    throw console.log(err instanceof Error ? err.message : String(err));
+  }
+};
+
+// This generates a list of predicted vs validating classes for the
+// target project and ML model.  This list should be reviewed and
+// copied into the TARGET_CLASSES section of analysisConfig.js
+//
+// Usage:
+// ```
+// STAGE=prod AWS_PROFILE=animl REGION=us-west-2  node ./src/scripts/generateValidating.js
+// ```
+const generateValidationListConfig = {
+  ANALYSIS_DIR: '/analysis',
+  PROJECT_ID: 'sci_biosecurity',
+  MODEL: 'mirav2',
+};
 const validationIdLists = await generateValidationList(generateValidationListConfig);
-
-const endTimer = performance.now();
-console.log(`Overall execution time: ${(endTimer - startTimer) / 1000} seconds`);
-
-console.log(validationIdLists);
+writeToFile(generateValidationListConfig, util.inspect(validationIdLists));
