620ec68fec050bd98224426448ca5247ef85b330
Nathaniel Rindlaub <nathanielrindlaub@gmail.com>
2026-06-04 10:47:55 -0700

    allow Project Members to upload images

--- src/api/auth/roles.ts
+++ src/api/auth/roles.ts
@@ -2,21 +2,21 @@ const MANAGER = 'project_manager';
 const MEMBER = 'project_member';
 // const OBSERVER = 'project_observer';
 
-const EXPORT_DATA_ROLES = [MANAGER, MEMBER];
-const WRITE_OBJECTS_ROLES = [MANAGER, MEMBER];
-const WRITE_VIEWS_ROLES = [MANAGER, MEMBER];
-const WRITE_COMMENTS_ROLES = [MANAGER, MEMBER];
-const READ_TASKS_ROLES = [MANAGER, MEMBER];
-const WRITE_PROJECT_ROLES = [MANAGER];
-const WRITE_IMAGES_ROLES = [MANAGER];
-const DELETE_IMAGES_ROLES = [MANAGER];
-const MANAGE_USERS_ROLES = [MANAGER];
-const WRITE_DEPLOYMENTS_ROLES = [MANAGER];
-const WRITE_AUTOMATION_RULES_ROLES = [MANAGER];
-const WRITE_CAMERA_REGISTRATION_ROLES = [MANAGER];
-const WRITE_CAMERA_SERIAL_NUMBER_ROLES = [MANAGER];
-const WRITE_DELETE_CAMERA_ROLES = [MANAGER];
-const WRITE_TAGS_ROLES = [MANAGER, MEMBER];
+const EXPORT_DATA_ROLES = [MANAGER, MEMBER]; // export annotation data
+const WRITE_OBJECTS_ROLES = [MANAGER, MEMBER]; // review images
+const WRITE_VIEWS_ROLES = [MANAGER, MEMBER]; // create views
+const WRITE_COMMENTS_ROLES = [MANAGER, MEMBER]; // write comments
+const READ_TASKS_ROLES = [MANAGER, MEMBER]; // read tasks
+const WRITE_PROJECT_ROLES = [MANAGER]; // manage project settings, labels, and tags
+const WRITE_IMAGES_ROLES = [MANAGER, MEMBER]; // upload images
+const DELETE_IMAGES_ROLES = [MANAGER]; // delete images
+const MANAGE_USERS_ROLES = [MANAGER]; // create users and assign roles
+const WRITE_DEPLOYMENTS_ROLES = [MANAGER]; // create deployments
+const WRITE_AUTOMATION_RULES_ROLES = [MANAGER]; // create automation rules
+const WRITE_CAMERA_REGISTRATION_ROLES = [MANAGER]; // register wireless cameras
+const WRITE_CAMERA_SERIAL_NUMBER_ROLES = [MANAGER]; // update camera serial number
+const WRITE_DELETE_CAMERA_ROLES = [MANAGER]; // delete cameras
+const WRITE_TAGS_ROLES = [MANAGER, MEMBER]; // apply tags to images
 
 export {
   READ_TASKS_ROLES,
